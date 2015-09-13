229a03634f293aba20f95e476c09f6d3a4d98782
alikelly001 <alexandrakelly001@gmail.com>
2015-09-13 17:19:08 +0100

    Adding site

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function askQuestions () {
+
+
+	var firstName = prompt('What is your first name?');
+	var lastName = prompt('What is your last name?');
+
+	var fullName = firstName + ' ' + lastName;
+	$ ('h2').text('Hello ' + fullName);
+
+
+
+	if (firstName.toLowerCase () == 'general' && lastName.toLowerCase () != 'assembly') {
+		console.log('Greetings General');
+	}
+
+
+	var age = prompt ('How old are you');
+	age = parseInt(age);
+
+	if (age >= 18) {
+		console.log('User is an adult.');
+	} else if (age >= 13) {
+		console.log('User is a teenager.');
+	} else {
+		console.log('User is a child.');
+	}
+
+
+	var faveColour = prompt('What\'s your favourite colour?').toLowerCase();
+
+	if (faveColour == 'red' ||
+		faveColour == 'blue' ||
+		faveColour == 'green' ||
+		faveColour == 'yellow') {
+		$('h2').css('color', faveColour);
+	}
+}
+
+
+// when the page is loaded
+$ (function() {
+
+	$('h2').on('click', askQuestions);
+
+	// find all the content and hide it
+	$('h3').next().hide();
+
+	// when the user clicks on an h3
+	$('h3') .on ('click', function() {
+
+		//find the next element and toggle it
+		$(this).next().slideToggle(1000);
+
+	});
+
+});
